094c446b272c9e8917180995d06a3d02dd41986c
Justin Taylor <26264269+sjustintaylor@users.noreply.github.com>
2021-03-27 15:46:16 +1000

    added test for reset

---
+++ src/tests/PRNG/reset.spec.ts
@@ -0,0 +1,15 @@
+import PRNG from "../../index";
+
+const seed = 1234567890;
+const prng = new PRNG(seed);
+const elementCount = 1000;
+
+it("should allow you to reset state", () => {
+  for (let i = 0; i < elementCount; i++) {
+    prng.randomInt();
+  }
+  const state = prng.saveState();
+  const next = prng.randomInt();
+  prng.reset(state);
+  expect(prng.randomInt()).toEqual(next);
+});
